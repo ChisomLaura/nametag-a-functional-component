4cc32b52dfd50d614cccbddf94ed84c428f25e28
chisom_laura <chisomonyegbula@Chisoms-MacBook.local>
2022-08-21 21:47:05 -0600

    initial commit

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const names = [
+  { person: "Laura", id: "001" },
+  { person: "Sophie", id: "002" },
+  { person: "Kosi", id: "003" },
+  { person: "Jesse", id: "004" },
+  { person: "Blessing", id: "005" },
+  { person: "Obi", id: "006" }
+];
